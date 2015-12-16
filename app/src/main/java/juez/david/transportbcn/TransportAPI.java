3a7b4ef0f9b8f1680fc4e6dc2c5fd9a9d27e103f
David Juez <david.juez@gmail.com>
2015-12-16 20:27:17 +0100

    posar be la key del mapa

--- app/src/main/java/juez/david/transportbcn/TransportAPI.java
+++ app/src/main/java/juez/david/transportbcn/TransportAPI.java
@@ -54,7 +54,7 @@ public class TransportAPI  {
     /**
      *  Mètode que obté el resultat de la connexió amb la api
      * @param tipus   el tipus de llista que es vol mostrar
-     */
+     * /
     public void transports(final TransportAdapter adapter, String tipus) {
         // fa la connexió amb la api amb la clau i el tipus de cerca que es vol
         Call<Transport> transportCall = service.getTransport(
@@ -65,7 +65,7 @@ public class TransportAPI  {
              * Mètode quant tot ha anat bé
              * @param response    La resposta que rep de la connexió
              * @param retrofit    El retrofit amb el que es connecta
-             */
+             * /
             @Override
             public void onResponse(Response<Transport> response, Retrofit retrofit) {
                 // la resposta la torna objecte, que hem creat prèviament,
@@ -79,11 +79,11 @@ public class TransportAPI  {
             /**
              * Mètode quant alguna cosa ha sortit malament
              * @param t
-             */
+             * /
             @Override
             public void onFailure(Throwable t) {
                 Log.e("Update movies", Arrays.toString(t.getStackTrace()));
             }
         });
-    }
+    }*/
 }
